299deaf04358a61fc1c59f588db99adc0594cf71
Jariullah <safijari@isu.edu>
2021-07-01 21:09:25 +0200

    Setup and readme

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name="jonky",
+    version="0.0.1",
+    description="jonky, Jari's conky",
+    author="Jariullah Safi",
+    author_email="safijari@isu.edu",
+    packages=find_packages(),
+    install_requires=["pycairo", "PyGObject"],
+)
