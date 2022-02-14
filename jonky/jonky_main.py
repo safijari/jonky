3cd29b53c40866391b1fc49f5e3bf41732d5d744
Jariullah <safijari@isu.edu>
2022-02-14 16:27:50 -0600

    Get rid of tk import

--- jonky/jonky_main.py
+++ jonky/jonky_main.py
@@ -4,7 +4,7 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GObject, Gdk
 import cairo
 import time
-from tkinter import Tk, Label
+# from tkinter import Tk, Label
 from PIL import Image as PImage
 from PIL import ImageTk as PImageTk
 from cairo import ImageSurface, Context, FORMAT_ARGB32
@@ -193,76 +193,76 @@ class JonkyPS:
         return self
 
 
-class JonkyTk(Tk):
-    def __init__(
-        self, w, h, items, update_period=1, is_background=False, *args, **kwargs
-    ):
-        super().__init__(*args, **kwargs)
-        self.xpos, self.ypos = 0, 0
-        self.drag_end = (0, 0)
-        self.move_flag = False
-        self.label = None
-        self.items = items
-        self.is_background = is_background
-        self.overrideredirect(is_background)
-        self.lower()
-        self.geometry("{}x{}".format(w, h))
-        self.update_period = update_period
-        self.bind("<Button-4>", self.scroll)
-        self.bind("<Button-5>", self.scroll)
-        self.bind("<Button1-Motion>", self.move)
-        self.bind("<ButtonRelease-1>", self.release)
-        self.zoom_level = 1.0
-        self.update()
-        self.mainloop()
+# class JonkyTk(Tk):
+#     def __init__(
+#         self, w, h, items, update_period=1, is_background=False, *args, **kwargs
+#     ):
+#         super().__init__(*args, **kwargs)
+#         self.xpos, self.ypos = 0, 0
+#         self.drag_end = (0, 0)
+#         self.move_flag = False
+#         self.label = None
+#         self.items = items
+#         self.is_background = is_background
+#         self.overrideredirect(is_background)
+#         self.lower()
+#         self.geometry("{}x{}".format(w, h))
+#         self.update_period = update_period
+#         self.bind("<Button-4>", self.scroll)
+#         self.bind("<Button-5>", self.scroll)
+#         self.bind("<Button1-Motion>", self.move)
+#         self.bind("<ButtonRelease-1>", self.release)
+#         self.zoom_level = 1.0
+#         self.update()
+#         self.mainloop()
 
-    def scroll(self, event):
-        if event.num == 4:
-            self.zoom_level += 0.1
-        else:
-            self.zoom_level -= 0.1
-        self.update(queue_another=False)
+#     def scroll(self, event):
+#         if event.num == 4:
+#             self.zoom_level += 0.1
+#         else:
+#             self.zoom_level -= 0.1
+#         self.update(queue_another=False)
 
-    def move(self, event):
-        if self.move_flag:
-            sx, sy = self.drag_start
-            ex, ey = self.drag_end
-            new_xpos, new_ypos = event.x, event.y
+#     def move(self, event):
+#         if self.move_flag:
+#             sx, sy = self.drag_start
+#             ex, ey = self.drag_end
+#             new_xpos, new_ypos = event.x, event.y
 
-            self.xpos = ex + new_xpos - sx
-            self.ypos = ey + new_ypos - sy
-        else:
-            self.move_flag = True
-            self.drag_start = (event.x, event.y)
+#             self.xpos = ex + new_xpos - sx
+#             self.ypos = ey + new_ypos - sy
+#         else:
+#             self.move_flag = True
+#             self.drag_start = (event.x, event.y)
 
-    def release(self, event):
-        self.move_flag = False
-        self.drag_end = (self.xpos, self.ypos)
+#     def release(self, event):
+#         self.move_flag = False
+#         self.drag_end = (self.xpos, self.ypos)
 
-    def update(self, queue_another=True):
-        if not self.is_background:
-            w = self.winfo_width()
-            h = self.winfo_height()
-        else:
-            w = self.winfo_screenwidth()
-            h = self.winfo_screenheight()
-            self.geometry("{}x{}".format(w, h))
-        main_group = (
-            Group(self.items).set_pose(self.xpos, self.ypos).set_scale(self.zoom_level)
-        )
-        self.ji = JonkyImage(w, h, [main_group], background_color=Color.named("black"))
-        self.ji.draw()
-        arr = self.ji.to_numpy()
-        _image_ref = PImageTk.PhotoImage(
-            PImage.fromarray(arr[:, :, :3][:, :, ::-1], "RGB")
-        )
-        if not self.label:
-            self.label = Label(self, image=_image_ref)
-            self.label.pack(expand=True, fill="both")
-        else:
-            # self.label.image = self._image_ref
-            self.label.configure(image=_image_ref)
-        self._image_ref = _image_ref
-        self.arr = arr
-        if queue_another:
-            self.after(int(self.update_period * 1000), self.update)
+#     def update(self, queue_another=True):
+#         if not self.is_background:
+#             w = self.winfo_width()
+#             h = self.winfo_height()
+#         else:
+#             w = self.winfo_screenwidth()
+#             h = self.winfo_screenheight()
+#             self.geometry("{}x{}".format(w, h))
+#         main_group = (
+#             Group(self.items).set_pose(self.xpos, self.ypos).set_scale(self.zoom_level)
+#         )
+#         self.ji = JonkyImage(w, h, [main_group], background_color=Color.named("black"))
+#         self.ji.draw()
+#         arr = self.ji.to_numpy()
+#         _image_ref = PImageTk.PhotoImage(
+#             PImage.fromarray(arr[:, :, :3][:, :, ::-1], "RGB")
+#         )
+#         if not self.label:
+#             self.label = Label(self, image=_image_ref)
+#             self.label.pack(expand=True, fill="both")
+#         else:
+#             # self.label.image = self._image_ref
+#             self.label.configure(image=_image_ref)
+#         self._image_ref = _image_ref
+#         self.arr = arr
+#         if queue_another:
+#             self.after(int(self.update_period * 1000), self.update)
