86456ca9ece3b10a7814f8482048fc62ea3ad087
Jariullah <safijari@isu.edu>
2023-05-11 16:23:32 -0500

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name="jonky",
-    version="0.0.4",
+    version="0.0.5",
     description="jonky, Jari's conky",
     author="Jariullah Safi",
     author_email="safijari@isu.edu",
